721249e214a1eebd51a57ce844b56462642ee248
Vinit Shahdeo <vinitshahdeo@gmail.com>
2023-04-09 18:14:50 +0530

    Updated the tentative date

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       </div>
       <div className="container">
         <div className="container_1">
-          <Timer deadline="April 5, 2023" />
+          <Timer deadline="May 30, 2023" />
         </div>
         <div className="container_2">
           <h3>
