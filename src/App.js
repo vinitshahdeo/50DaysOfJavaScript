8541cfbe6da99e853c92793c5b9b444c982d7843
Vinit Shahdeo <vinitshahdeo@gmail.com>
2023-07-16 22:02:17 +0530

    Updated start date

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ function App() {
       </div>
       <div className="container">
         <div className="container_1">
-          <Timer deadline="July 1, 2023" />
+          <Timer deadline="October 1, 2023" />
         </div>
         <div className="container_2">
           <h3>
-            Calling out 📣 participants, mentors, and communities for an open source initiative—join us to make a difference. More details 📩 to follow!
+            Calling out 📣 participants, mentors, and communities for an open-source initiative—join us to make a difference. More details 📩 to follow!
           </h3>
         </div>
       </div>
